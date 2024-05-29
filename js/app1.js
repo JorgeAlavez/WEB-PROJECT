47323bdef00199a4d28bcb25a662f153aeaf721e
Axel <domo_gabo@hotmail.com>
2024-05-28 20:16:18 -0600

    Tarjetas terminadas

--- js/app1.js
+++ js/app1.js
@@ -53,22 +53,17 @@ document.addEventListener("DOMContentLoaded", () => {
                     const img = new Image();
                     img.src = imagenUrl;
                     img.onload = () => {
-                        if (img.height <= 500 && !imageFound) {
+                        if (img.height <= 1000 && !imageFound) {
                             productosHTML += `
-                                <div class="card mb-3" style="width: 50rem;">
-                                    <div class="row g-0">
-                                        <div class="col-md-4">
-                                            <img src="${imagenUrl}" class="img-fluid rounded-start" alt="${objeto.title}">
-                                        </div>
-                                        <div class="col-md-8">
-                                            <div class="card-body">
-                                                <h5 class="card-title">${objeto.id}</h5>
-                                                <p class="card-text">${objeto.description}</p>
-                                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div class="col">
+                            <div class="card h-100" style="width: 350px;">
+                              <img src="${imagenUrl}" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                <h5 class="card-title">${objeto.id}</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                              </div>
+                            </div>
+                          </div>
                             `;
                             divProductos.innerHTML = productosHTML;
                             imageFound = true; // Actualizar flag indicando que se encontró una imagen válida
@@ -77,25 +72,20 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             });
 
-            if(objeto.id == 42 || objeto.id == 68){
-                productosHTML += `
-                    <div class="card mb-3" style="width: 50rem;">
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src="${objeto.thumbnail}" class="img-fluid rounded-start ajustarCards100" alt="${objeto.title}">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">${objeto.id}</h5>
-                                    <p class="card-text">${objeto.description}</p>
-                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                `;
-                divProductos.innerHTML = productosHTML;
-            }
+            // if(objeto.id == 42 || objeto.id == 68){
+            //     productosHTML += `
+            //     <div class="col">
+            //     <div class="card h-100">
+            //       <img src="${imagenUrl}" class="card-img-top" alt="...">
+            //       <div class="card-body">
+            //         <h5 class="card-title">${objeto.id}</h5>
+            //         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            //       </div>
+            //     </div>
+            //   </div>
+            //     `;
+            //     divProductos.innerHTML = productosHTML;
+            // }
         });
 
 });
